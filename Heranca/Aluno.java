4668ad6fdb1155f3d318bd8d8b1b29cfea210caa
Lettycia Vitória <163659294+LettyciaDev@users.noreply.github.com>
2025-03-14 15:27:14 +0000

    herança - super

---
+++ Heranca/Aluno.java
@@ -0,0 +1,10 @@
+public class Aluno extends Pessoa{
+    public Aluno(String no, int idd, double alt){
+        super(no,idd,alt);
+    }
+
+    @Override 
+    public String toString(){
+        return super.toString();
+    }
+}
---
+++ Heranca/App.java
@@ -0,0 +1,10 @@
+public class App{
+    public static void main(String[] args){
+        Pessoa p1 = new Pessoa("Leticia", 10, 1.70);
+        p1.toString();
+
+        Aluno a1 = new Aluno("Joao", 20, 1.80);
+
+        a1.toString();
+    }
+}
