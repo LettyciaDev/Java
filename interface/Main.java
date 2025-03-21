fdab53e33c947b900cc1eb32a707c4174f9112e6
Lettycia Vitória <163659294+LettyciaDev@users.noreply.github.com>
2025-03-21 15:19:26 +0000

    interface - parte 2

---
+++ interface/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args) {
+        Comparable[] ep = new Empregado[4];
+        ep[0] = new Empregado(2, "João");
+        ep[1] = new Empregado(4, "Maria");
+        ep[2] = new Empregado(3, "Anna");
+        ep[3] = new Empregado(55, "José");
+
+        ordenarSelecao(vec, vec.length);
+    }
+
+    public static void ordenarSelecao(Comparable[] vec, int n){
+        for(int i = 0; i <= n-2; i++){
+            int min = i;
+            for (int j < 0; )
+        }
+    }
+}
